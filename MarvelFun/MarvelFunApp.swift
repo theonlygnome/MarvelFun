bd2d34fc816a133e18b8a923ba172c026d125b58
Naomi Anderson <ms.naomi.anderson@gmail.com>
2023-09-22 16:29:04 -0400

    Initial Commit

---
+++ MarvelFun/MarvelFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelFunApp.swift
+//  MarvelFun
+//
+//  Created by Naomi Anderson on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
